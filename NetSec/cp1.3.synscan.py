63e24f740bcd2dd1f0be37b7e0867eadae21f030
aa <aa@aa.com>
2021-04-11 01:11:50 +0800

    Add cp1

--- NetSec/cp1.3.synscan.py
+++ NetSec/cp1.3.synscan.py
@@ -1,16 +1,33 @@
 from scapy.all import *
 
 import sys
+from multiprocessing import Pool
+
 
 def debug(s):
     print('#{0}'.format(s))
     sys.stdout.flush()
+
+
+def check_pair(conf):
+    ip, iface, port = conf
+    # debug("Scanning: {}:{}".format(ip, port))
+    syn = IP(dst=ip)/TCP(dport=port, flags="S")
+    syn_ack = sr1(syn, timeout=10, verbose=0)
+    if syn_ack.getlayer(TCP).flags == "SA":
+        print("{}:{}".format(ip, port))
+    else:
+        # debug("Closed: {}:{}".format(ip, port))
+        return
+
 
 if __name__ == "__main__":
     conf.iface = sys.argv[1]
     ip_addr = sys.argv[2]
 
     my_ip = get_if_addr(sys.argv[1])
-    
-    # SYN scan
 
+    # SYN scan
+    configs = map(lambda x: (ip_addr, conf.iface, x), range(1, 1025))
+    with Pool(32) as p:
+        p.map(check_pair, configs)
